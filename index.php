5c6438d1d2972659fa3b84ee0fc7c2b53737f5bb
raydanielg <airezra2+1@gmail.com>
2025-11-09 06:11:00 +0300

    commit

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 
 define('LARAVEL_START', microtime(true));
 
-require __DIR__.'/../vendor/autoload.php';
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
+    require $maintenance;
+}
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+// Register the Composer autoloader...
+require __DIR__.'/vendor/autoload.php';
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once __DIR__.'/bootstrap/app.php';
 
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+$app->handleRequest(Request::capture());
