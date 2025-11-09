b0b1319c1cbe6ef0b841f1ff54bfd0f6d513bc5a
raydanielg <airezra2+1@gmail.com>
2025-11-09 06:07:48 +0300

    commit

--- index.php
+++ index.php
@@ -1,24 +1,17 @@
 <?php
 
-/**
- * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
- */
-
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
-    require $maintenance;
-}
+require __DIR__.'/../vendor/autoload.php';
 
-// Register the Composer autoloader...
-require __DIR__.'/vendor/autoload.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/bootstrap/app.php';
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-$app->handleRequest(Request::capture());
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
